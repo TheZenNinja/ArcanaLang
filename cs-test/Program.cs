1be744c100ebeb8df01ee3b73b79fd90e3a57c62
= <=>
2023-11-27 14:43:10 -0500

    messing around

---
+++ cs-test/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace cs_test
+{
+    public class Program
+    {
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ lexer-test/lexer.lua
@@ -0,0 +1,7 @@
+local tokens = {}
+
+local file = io.open("test.arc", "r")
+
+local line = file:read()
+
+print(line)
